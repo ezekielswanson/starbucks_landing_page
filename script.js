8a0b91d9990f4ad792ba407050e3aa173283ce25
ezekielswanson <swansonezekiel@gmail.com>
2021-11-30 18:29:06 -0600

    Updated js file.

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ const btn = document.getElementById("menu-btn");
 const hiddenBtn = document.getElementById('menu');
 
 function menuToggle() {
-    
+
     btn.classList.toggle('open');
     hiddenBtn.classList.toggle('hidden');
     document.body.classList.toggle('no-scroll');
     
 }
-
-
 
 btn.addEventListener('click', menuToggle)
